bfb88a776b60c88dfa962b14e74d262ef3912b17
ArvidPettersson2C <arvid.pettersson@elev.ga.ntig.se>
2024-03-26 22:15:05 +0100

    2024-03-26

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-// hamburgermeny
 const burger = document.getElementById('burger')
-const linkList = document.getElementById('burger_link_list')
+const linkList = document.getElementById('link_list')
 
 function toggleMenu() {
+
     linkList.classList.toggle('links_visible')
     burger.classList.toggle('burger_in_x_form')
+
 }
 
 burger.addEventListener('click', toggleMenu)
